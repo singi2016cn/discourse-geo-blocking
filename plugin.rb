eae1fa5a4db10b0bc7093edf75b87a86108e7ffd
singi.qin <singi.qin@quectel.com>
2026-04-09 15:03:12 +0800

    feat: add ip whitelist

--- plugin.rb
+++ plugin.rb
@@ -40,6 +40,10 @@ after_initialize do
       sr = super
       return sr unless sr == :skip
 
+      if SiteSetting.geo_moderating_country_region_whitelist.split("|").include?(manager.user.custom_fields["geoblocking_last_ip_address"])
+        return
+      end
+
       origin = ::GeoBlocking::Lookup.is_moderated?(manager.user.custom_fields["geoblocking_last_ip_address"])
       if origin
         return I18n.t("geo_blocking.moderate_reason", { origin: origin })
@@ -56,15 +60,25 @@ after_initialize do
     return if request.fullpath.start_with?("/admin/", "/message-bus/", "/theme-javascripts/", "/stylesheets/", "/letter_avatar_proxy/", "/svg-sprite/", "/extra-locales/")
 
     ip = request.env["HTTP_X_REAL_IP"] || request.env["REMOTE_ADDR"]
+    Rails.logger.info("[GeoBlocking] Checking IP: #{ip} for path: #{request.fullpath}")
 
     user = current_user
     if user
       user.custom_fields["geoblocking_last_ip_address"] = ip
       user.save_custom_fields(true)
+      Rails.logger.info("[GeoBlocking] Saved IP address for user id: #{user.id}")
+    end
+
+    Rails.logger.info("[GeoBlocking] IPs #{SiteSetting.geo_blocking_country_region_whitelist} is whitelisted, allowing access")
+    if SiteSetting.geo_blocking_country_region_whitelist.split("|").include?(ip)
+      Rails.logger.info("[GeoBlocking] IP #{ip} is whitelisted, allowing access")
+      return
     end
 
     reason = ::GeoBlocking::Lookup.is_blocked?(ip)
     return unless reason
+
+    Rails.logger.warn("[GeoBlocking] Blocking IP: #{ip}, reason: #{reason}")
 
     if SiteSetting.geo_blocking_detailed_reason
       rescue_discourse_actions(:unavailable, 451, {custom_message: "geo_blocking.error_451_detailed",  custom_message_params: { origin: reason }})
